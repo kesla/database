ca699d2842de57f852f78ee11870bff6cbc64366
David Björklund <david.bjorklund@gmail.com>
2014-08-18 01:40:27 -0400

    remove mentioning to medeadown

--- test.js
+++ test.js
@@ -1,28 +1,28 @@
 var test       = require('tape')
   , testCommon = require('abstract-leveldown/testCommon')
-  , medeaDOWN  = require('./db')
+  , factory  = require('./db')
   , testBuffer = require('fs').readFileSync(__filename)
   , db
 
 /*** compatibility with basic LevelDOWN API ***/
 
-require('abstract-leveldown/abstract/leveldown-test').args(medeaDOWN, test, testCommon)
+require('abstract-leveldown/abstract/leveldown-test').args(factory, test, testCommon)
 
-require('abstract-leveldown/abstract/open-test').all(medeaDOWN, test, testCommon)
+require('abstract-leveldown/abstract/open-test').all(factory, test, testCommon)
 
-require('abstract-leveldown/abstract/del-test').all(medeaDOWN, test, testCommon)
+require('abstract-leveldown/abstract/del-test').all(factory, test, testCommon)
 
-require('abstract-leveldown/abstract/get-test').all(medeaDOWN, test, testCommon)
+require('abstract-leveldown/abstract/get-test').all(factory, test, testCommon)
 
-require('abstract-leveldown/abstract/put-test').all(medeaDOWN, test, testCommon)
+require('abstract-leveldown/abstract/put-test').all(factory, test, testCommon)
 
-require('abstract-leveldown/abstract/put-get-del-test').all(medeaDOWN, test, testCommon, testBuffer)
+require('abstract-leveldown/abstract/put-get-del-test').all(factory, test, testCommon, testBuffer)
 
-require('abstract-leveldown/abstract/batch-test').all(medeaDOWN, test, testCommon)
-require('abstract-leveldown/abstract/iterator-test').all(medeaDOWN, test, testCommon)
+require('abstract-leveldown/abstract/batch-test').all(factory, test, testCommon)
+require('abstract-leveldown/abstract/iterator-test').all(factory, test, testCommon)
 
-require('abstract-leveldown/abstract/chained-batch-test').all(medeaDOWN, test, testCommon)
+require('abstract-leveldown/abstract/chained-batch-test').all(factory, test, testCommon)
 
-require('abstract-leveldown/abstract/close-test').close(medeaDOWN, test, testCommon)
+require('abstract-leveldown/abstract/close-test').close(factory, test, testCommon)
 
-require('abstract-leveldown/abstract/ranges-test').all(medeaDOWN, test, testCommon)
+require('abstract-leveldown/abstract/ranges-test').all(factory, test, testCommon)
